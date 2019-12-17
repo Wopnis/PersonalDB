41d6cd5561c0ea6d7ee069b7fd3eca04a1f9c4b2
Konstantin800306 <wopnis2016@outlook.com>
2019-12-17 20:47:59 +0300

    first commit-all_routes_workes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
